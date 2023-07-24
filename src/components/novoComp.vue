0fb19af8fb37849ad9c1f64170f5b9b3b8302a4e
henrique <henrique.florencio307@gmail.com>
2023-07-24 20:48:36 -0300

    primeiro commit

---
+++ src/components/novoComp.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    const ok: string = "teste"
+
+    defineProps<{
+        batata?: string
+    }>()
+</script>
+
+<template>
+    <div v-if="batata">uma prop foi passada {{ batata }}</div>
+    <div>{{ ok }}</div>
+</template>
